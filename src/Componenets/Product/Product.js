3a0ec6bb0f8bd75702bbcda827e248e484b7a56f
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-03-27 18:01:17 +0600

    button added

--- src/Componenets/Product/Product.js
+++ src/Componenets/Product/Product.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const { name, img } = props.product;
+    const { name, img, price } = props.product;
     return (
         <div className='product'>
-            <img src={img} alt="" />
-            <h2>Name: {name} </h2>
+            <div>
+                <img src={img} alt="" />
+                <h3>Name: {name} </h3>
+                <p>Price: ${price}</p>
+            </div>
+            <button className='cart-btn'>Add To Cart</button>
         </div>
+
+
     );
 };
 
